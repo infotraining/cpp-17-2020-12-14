c174096ecc850beac4674cbba1e6ecae11e61a5a
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-12-15 13:33:51 +0100

    rvo

--- bugfixes/tests.cpp
+++ bugfixes/tests.cpp
@@ -51,3 +51,104 @@ TEST_CASE("C++ & bytes")
         std::cout << "result in bytes: " << std::to_integer<uint32_t>(result) << "\n";
     }
 }
+
+TEST_CASE("auto + {}")
+{
+    int x1 = 10;
+    int x2{10};
+
+    auto a1 = 10; // int
+    auto a2{10};  // int
+    //auto a3{1, 2, 3}; // ERROR - ill-formed
+
+    auto lst1 = {1, 2, 3}; // std::initializer_list<int>
+    auto lst2 = {1};       // std::initializer_list<int>
+}
+
+void foo1()
+{
+}
+
+void foo2() noexcept
+{
+}
+
+template <typename F1, typename F2>
+void call(F1 f1, F2 f2)
+{
+    f1();
+    f2();
+}
+
+TEST_CASE("noexcept")
+{
+    static_assert(!std::is_same_v<decltype(foo1), decltype(foo2)>);
+
+    void (*ptr_fun)() noexcept = nullptr;
+
+    ptr_fun = foo2;
+
+    call(foo1, foo2);
+}
+
+std::vector<int> create_vector_rvo(size_t n)
+{
+    return std::vector<int>(n, -1);  // prvalue
+}
+
+std::vector<int> create_vector_nrvo(size_t n)
+{
+    vector<int> vec(n);
+    std::iota(begin(vec), end(vec), 0);
+    
+    return vec; // lvalue
+}
+
+TEST_CASE("rvo vs. nrvo")
+{
+    std::vector<int> data = create_vector_rvo(1'000'000);
+}
+
+struct CopyMoveDisabled
+{
+    vector<int> data;
+
+    CopyMoveDisabled(std::initializer_list<int> lst)
+        : data {lst}
+    {
+    }
+
+    CopyMoveDisabled(const CopyMoveDisabled&) = delete;
+    CopyMoveDisabled& operator=(const CopyMoveDisabled&) = delete;
+    CopyMoveDisabled(CopyMoveDisabled&&) = delete;
+    CopyMoveDisabled& operator=(CopyMoveDisabled&&) = delete;
+    ~CopyMoveDisabled() = default;
+};
+
+CopyMoveDisabled create_impossible()
+{
+    return CopyMoveDisabled{1, 2, 3};
+}
+
+// CopyMoveDisabled create_cmd_nrvo()
+// {
+//     CopyMoveDisabled result{1, 2, 3};
+//     return result; // illegal - CopyMoveDisabled must be copyable or moveable
+// }
+
+void use_impossible(CopyMoveDisabled arg)
+{
+    std::cout << "Using object: ";
+    for(const auto& item : arg.data)
+        std::cout << item << " ";
+    std::cout << "\n";
+}
+
+TEST_CASE("returning & passing CopyMoveDisabled")
+{
+    CopyMoveDisabled cmd = create_impossible();
+
+    REQUIRE(cmd.data == vector{1, 2, 3});
+
+    use_impossible(create_impossible());
+}
